309aac8f3c3fb5938bec606d55b9794d9a7adfc9
Paulo_camara/Dev <99374349+26411988@users.noreply.github.com>
2024-03-22 14:30:57 +0000

    atualização

---
+++ exercicios.cpp
@@ -0,0 +1,12 @@
+//   exercicio imprimir nome e idade
+
+#include <iostream>
+int main () {
+printf("Paulo Henrique Câmara \n");
+printf (" 35 anos  Since 1988");
+
+return 0;
+
+}
+
+
---
+++ exercioSoma.cpp
@@ -0,0 +1,28 @@
+//  2   executando soma entre dois numeros
+#include <iostream>
+int main() {
+// defini os numeros para a soma
+int num1=10;
+int num2=5;
+
+// Ralizando a  Operação
+int soma=  num1 + num2;
+int subtracao= num1 - num2;
+// Imprimindo a soma
+
+printf("soma: %d + %d = %d\n", num1,num2,soma);
+printf("subtracao: %d - %d = %d\n", num1,num2,subtracao);
+
+return 0;
+
+
+
+
+
+
+
+}
+
+
+
+
